bcdccb7134ffee2e564a0fa6c259158cb8ab21ac
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2022-11-06 23:56:17 -0500

    commit

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,10 +1,13 @@
 import "./index.css"
 import React from "react";
-import postsArray from './posts.json';
+// import postsArray from './posts.json';
 import PostSummaryItem
   from "./post-summary-item";
+import {useSelector} from "react-redux";
+
 
 const PostSummaryList = () => {
+const postsArray = useSelector(state => state.tuits)
  return(
 <div className="contentBox">
 ${
--- src/tuiter/tuits/tuits-list.js
+++ src/tuiter/tuits/tuits-list.js
@@ -1,20 +1,21 @@
-import "../index.css"
+import "../home/index.css"
 import React from "react";
 // import homePostsArray from './homepost.json';
-import HomePostItem from "./home-post-item";
-
+import TuitItem from "./tuit-item";
 import {useSelector} from "react-redux";
 
-const HomePostList = () => {
-  const homePostsArray = useSelector(state => state.tuits)
+
+const TuitsList = () => {
+    const homePostsArray = useSelector(state => state.tuits)
+
  return(
 <div>
 {
      homePostsArray.map(post =>
-        <HomePostItem
+        <TuitItem
           key={post._id} post={post}/> )
  }
 </div>
  );
 };
-export default HomePostList;
+export default TuitsList;
